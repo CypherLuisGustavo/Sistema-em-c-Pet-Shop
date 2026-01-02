c76386d58c06f27a941882b91b8f627fe9bce7f6
CypherLuisGustavo <luisgustavoo2003@gmail.com>
2026-01-02 12:59:50 -0300

    Update agendamento.h

--- include/agendamento.h
+++ include/agendamento.h
@@ -1,1 +1,13 @@
+#ifndef AGENDAMENTOS_H
+#define AGENDAMENTOS_H
 
+#include "dados.h"
+//*declaração de agendamendos Header*//
+
+void cadastrar_agendamentos(PetAgendamento lista[],int *total);
+void listar_agendamentos(PetAgendamento lista[],int total);
+int buscar_agendamentos(PetAgendamento lista[], int total, char nome_pet[]);
+void editar_agendamento(PetAgendamento lista[], int total);
+void excluir_agendamento(PetAgendamento lista[], int *total);
+
+#endif
